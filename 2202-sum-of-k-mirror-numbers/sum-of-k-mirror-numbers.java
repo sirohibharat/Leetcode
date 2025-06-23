a79d87717dda5ef91dc6bd38fee3d6144e3e7fb5
Bharat Sirohi <151562072+sirohibharat@users.noreply.github.com>
2025-06-23 10:02:12 +0530

    Time: 257 ms (64.29%) | Memory: 44.5 MB (55.36%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.java
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.java
@@ -1,62 +1,32 @@
 class Solution {
-    
-    // Check if a string is a palindrome
-    private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        
-        while (left <= right) {
-            if (s.charAt(left++) != s.charAt(right--)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    public long kMirror(int base, int count) {
+        long sum = 0; 
+        for (int length = 1; ; ++length) {
+            int start = (int) Math.pow(10, (length - 1) / 2);
+            int end = (int) Math.pow(10, (length + 1) / 2);
 
-    // Convert a number to base-k string representation
-    private String convertToBaseK(long num, int k) {
-        if (num == 0) return "0";
-        
-        StringBuilder res = new StringBuilder();
-        while (num > 0) {
-            res.append(num % k);
-            num /= k;
-        }
-        return res.toString();
-    }
-
-    public long kMirror(int k, int n) {
-        long sum = 0;
-        int length = 1;
-
-        while (n > 0) {
-            int halfLength = (length + 1) / 2;
-            long minNum = (long)Math.pow(10, halfLength - 1);
-            long maxNum = (long)Math.pow(10, halfLength) - 1;
-
-            for (long num = minNum; num <= maxNum; num++) {
-                String firstHalf = Long.toString(num);
-                String secondHalf = new StringBuilder(firstHalf).reverse().toString();
-                
-                String fullPalindrome;
-                if (length % 2 == 0) {
-                    fullPalindrome = firstHalf + secondHalf;
-                } else {
-                    fullPalindrome = firstHalf + secondHalf.substring(1);
+            for (int i = start; i < end; i++) {
+                long palindrome = i;
+                for (int j = length % 2 == 0 ? i : i / 10; j > 0; j /= 10) {
+                    palindrome = palindrome * 10 + j % 10; 
                 }
-
-                long palNum = Long.parseLong(fullPalindrome);
-                String baseK = convertToBaseK(palNum, k);
-
-                if (isPalindrome(baseK)) {
-                    sum += palNum;
-                    n--;
-                    if (n == 0) {
-                        return sum;
+                String baseRepresentation = Long.toString(palindrome, base);
+                if (isPalindrome(baseRepresentation.toCharArray())) {
+                    sum += palindrome; 
+                    if (--count == 0) { 
+                        return sum; 
                     }
                 }
             }
-            length++;
         }
-        return sum;
+    }
+
+    private boolean isPalindrome(char[] charArray) {
+        for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
+            if (charArray[i] != charArray[j]) {
+                return false; 
+            }
+        }
+        return true;
     }
 }
