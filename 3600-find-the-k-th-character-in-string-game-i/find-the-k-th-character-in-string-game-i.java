028e0b1243f4548b170d6c39822a88195069b80e
Bharat Sirohi <151562072+sirohibharat@users.noreply.github.com>
2025-07-04 02:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (85.12%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,14 +1,11 @@
 class Solution {
     public char kthCharacter(int k) {
-        // Find the minimum number of operations needed
         int length = 1;
         int operations = 0;
         while (length < k) {
             length *= 2;
             operations++;
         }
-        
-        // Work backwards to find the number of transformations
         int transformations = 0;
         int pos = k;
         int currentLength = length;
@@ -16,19 +13,15 @@ class Solution {
         for (int op = operations; op > 0; op--) {
             int halfLength = currentLength / 2;
             if (pos > halfLength) {
-                // This character came from the transformed part
                 transformations++;
                 pos -= halfLength;
             }
             currentLength = halfLength;
         }
-        
-        // The original character was 'a', apply transformations
         char result = 'a';
         for (int i = 0; i < transformations; i++) {
             result = (char) ((result - 'a' + 1) % 26 + 'a');
         }
-        
         return result;
     }
 }
