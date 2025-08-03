5c3c227dd3e26a4c826c697722e85536a0365aad
Bharat Sirohi <151562072+sirohibharat@users.noreply.github.com>
2025-08-03 12:31:04 +0530

    Time: 3 ms (82.74%) | Memory: 49.8 MB (40.18%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,14 +8,14 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     count++;
                 }
             }
         }
         return count;
     }
-    public void dfs(char[][] grid,int i,int j){
+    public void bfs(char[][] grid,int i,int j){
         if(i<0 || j<0 || i>=n || j>=m){
             return;
         }
@@ -23,9 +23,9 @@ class Solution {
             return;
         }
         grid[i][j]='2';
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
+        bfs(grid,i-1,j);
+        bfs(grid,i+1,j);
+        bfs(grid,i,j-1);
+        bfs(grid,i,j+1);
     }
 }
